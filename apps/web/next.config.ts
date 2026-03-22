c425ac17fa028f72d40b201bb9c2ed60b1475b8e
CorentinDelorme <CorentinDelorme@users.noreply.github.com>
2026-03-22 15:13:58 +0100

    Fix turbopack root

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,6 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
   webpack: (webpackConfig) => {
@@ -17,7 +13,7 @@ const nextConfig: NextConfig = {
     return webpackConfig
   },
   turbopack: {
-    root: path.resolve(dirname),
+    root: path.join(__dirname, '../../'),
   },
 }
 
